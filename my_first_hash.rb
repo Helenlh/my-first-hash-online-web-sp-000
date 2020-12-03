b7ea9bd15abb9f9ad07d317c88050cc7653d01f1
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-03 02:09:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -52,10 +52,10 @@ shipping_manifest ["muskets"] = "2"
  
  
  
- shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                  "muskets" => 2, 
-                  "gunpowder" => 4
- }
+# shipping_manifest = {
+#                   "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                   "muskets" => 2, 
+#                   "gunpowder" => 4
+# }
