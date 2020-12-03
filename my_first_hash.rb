4cfdd24c40b429541acb796f060b90e43c77d5ed
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-03 02:05:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -46,13 +46,7 @@ shipping_manifest ["muskets"] = "2"
   shipping_manifest ["gun powder"] = "4"
 
   # return the shipping_manifest hash below
- shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                  "muskets" => 2, 
-                  "gunpowder" => 4
- }
+ 
  end 
   shipping_manifest 
  
